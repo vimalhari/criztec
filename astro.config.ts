6d650cdb8138711d7e5a68b0d5597e8e945b0ba8
vimalhari <vimal_hari@hotmail.com>
2026-01-21 09:13:16 +0000

    tailwind update

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
@@ -33,9 +33,6 @@ export default defineConfig({
   }),
 
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
     sitemap(),
     mdx({
       remarkPlugins: [readingTimeRemarkPlugin],
@@ -102,6 +99,7 @@ export default defineConfig({
   },
 
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         '~': path.resolve(__dirname, './src'),
